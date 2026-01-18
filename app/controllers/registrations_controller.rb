a2a1b924342c787c8cbb32bc4e4afc97b4cc7428
pratiamalden <pratiamalden@gmail.com>
2026-01-18 00:41:34 +0000

    Clean up RegistrationsController and fix signup bugs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,23 +1,15 @@
 class RegistrationsController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
   end
 
   def create
-    @user = USer.new(registration_params)
+    @user = User.new(registration_params)
     if @user.save
       login @user
       redirect_to root_path, notice: "Account created successfuly!"
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
